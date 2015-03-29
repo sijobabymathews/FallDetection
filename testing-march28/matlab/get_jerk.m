deef8722b36e66ce03b375902d0ac19fa93f1347
Gurneet Arora <gurneet.k.arora@gmail.com>
2015-03-28 22:20:03 -0400

    adding 4 second acceleration delay

--- testing-march28/matlab/get_jerk.m
+++ testing-march28/matlab/get_jerk.m
@@ -1,11 +1,16 @@
-function [ jerk ] = get_jerk( mag )
+function [ jerk, jerk4 ] = get_jerk( mag )
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 
-    jerk = zeros(length(mag),1);
-
-    for i = 2:length(mag)
+    jerk = zeros(length(mag)-1,1);
+    jerk4 = zeros(length(mag)-4,1);
+    
+    for i = 2:1:length(mag)
         jerk(i) = mag(i) - mag(i-1);
+    end
+    
+    for i = 5:1:length(mag)
+        jerk4(i) = mag(i) - mag(i-4);
     end
 end
 
